255d651f7139aa9bb3a7f6e84614f424ae1da1df
Hiroshi Yokoi <yokoi.hiroshi@po.ntts.co.jp>
2013-11-21 19:26:22 +0900

    modified sample code and document

--- ja/source/sources/simple_switch_rest_13.py
+++ ja/source/sources/simple_switch_rest_13.py
@@ -1,4 +1,6 @@
 import json
+import logging
+
 from ryu.app import simple_switch_13
 from webob import Response
 from ryu.controller import ofp_event
@@ -7,6 +9,7 @@ from ryu.controller.handler import set_ev_cls
 from ryu.app.wsgi import ControllerBase, WSGIApplication, route
 from ryu.lib import dpid as dpid_lib
 
+LOG = logging.getLogger('simple_switch_rest_13')
 simple_switch_instance_name = 'simple_switch_api_app'
 url = '/v1/simpleswitch/mactable/{dpid}'
 
@@ -26,54 +29,60 @@ class SimpleSwitchRest13(simple_switch_13.SimpleSwitch13):
         datapath = ev.msg.datapath
         self.switches[datapath.id] = datapath
 
-    def set_mac_to_port(self, dpid, mac_port_dict):
-        datapath = self.switches.get(dpid, None)
+    def set_mac_to_port(self, dpid, entry):
+        mac_table = self.mac_to_port.setdefault(dpid, {})
+        datapath = self.switches.get(dpid)
+
+        entry_port = entry['port']
+        entry_mac = entry['mac']
 
         if datapath is not None:
             parser = datapath.ofproto_parser
-            ports = mac_port_dict.values()
-            for in_port in ports:
-                for mac, port in mac_port_dict.items():
-                    if port != in_port:
-                        dst_mac = mac
-                        out_port = port
-                        actions = [parser.OFPActionOutput(out_port)]
-                        match = parser.OFPMatch(in_port=in_port, eth_dst=dst_mac)
-                        self.add_flow(datapath, 1, match, actions)
+            if entry_port not in mac_table.values():
 
-        mac_table = self.mac_to_port.get(dpid, {})
-        mac_table.update(mac_port_dict)
-        self.mac_to_port[dpid] = mac_table
-        return {'status' : 'ok'}
+                for mac, port in mac_table.items():
 
+                    # from known device to new device
+                    actions = [parser.OFPActionOutput(entry_port)]
+                    match = parser.OFPMatch(in_port=port, eth_dst=entry_mac)
+                    self.add_flow(datapath, 1, match, actions)
+
+                    # from new device to known device
+                    actions = [parser.OFPActionOutput(port)]
+                    match = parser.OFPMatch(in_port=entry_port, eth_dst=mac)
+                    self.add_flow(datapath, 1, match, actions)
+
+                mac_table.update({entry_mac : entry_port})
+        return mac_table
 
 class SimpleSwitchController(ControllerBase):
+
     def __init__(self, req, link, data, **config):
         super(SimpleSwitchController, self).__init__(req, link, data, **config)
         self.simpl_switch_spp = data[simple_switch_instance_name]
 
     @route('simpleswitch', url, methods=['GET'], requirements={'dpid': dpid_lib.DPID_PATTERN})
     def list_mac_table(self, req, **kwargs):
+
         simple_switch = self.simpl_switch_spp
-        mac_table = {}
-
-        if 'dpid' in kwargs:
-            dpid = dpid_lib.str_to_dpid(kwargs['dpid'])
-            mac_table = simple_switch.mac_to_port.get(dpid, {})
-
+        dpid = dpid_lib.str_to_dpid(kwargs['dpid'])
+        mac_table = simple_switch.mac_to_port.get(dpid, {})
         body = json.dumps(mac_table)
         return Response(content_type='application/json', body=body)
 
     @route('simpleswitch', url, methods=['PUT'], requirements={'dpid': dpid_lib.DPID_PATTERN})
     def put_mac_table(self, req, **kwargs):
-        simple_switch = self.simpl_switch_spp
-        result = {}
-        if 'dpid' in kwargs:
-            dpid = dpid_lib.str_to_dpid(kwargs['dpid'])
-            mac_table = eval(req.body)
-            result = simple_switch.set_mac_to_port(dpid, mac_table)
 
-        body = json.dumps(result)
-        return Response(content_type='application/json', body=body)
+        simple_switch = self.simpl_switch_spp
+        dpid = dpid_lib.str_to_dpid(kwargs['dpid'])
+        new_entry = eval(req.body)
+
+        try:
+            mac_table = simple_switch.set_mac_to_port(dpid, new_entry)
+            body = json.dumps(mac_table)
+            return Response(content_type='application/json', body=body)
+        except Exception as e:
+            LOG.error(e.message);
+            return Response("internal server error ", status=500)
 
 
