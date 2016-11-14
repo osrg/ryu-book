1e1e3a35790e1e5f8db92f06041caaf65d74449c
Ian Y. Choi <ianyrchoi@gmail.com>
2016-11-14 15:24:07 +0000

    ko: link_aggregation: Fix a import Link error problem
    (from commit 18109113ef8e004d7b5f5241c3863a7fadb448b1)

--- ko/source/sources/link_aggregation.py
+++ ko/source/sources/link_aggregation.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 from mininet.cli import CLI
-from mininet.link import Link
 from mininet.net import Mininet
 from mininet.node import RemoteController
 from mininet.term import makeTerm
@@ -18,11 +17,11 @@ if '__main__' == __name__:
     h3 = net.addHost('h3', mac='00:00:00:00:00:23')
     h4 = net.addHost('h4', mac='00:00:00:00:00:24')
 
-    Link(s1, h1)
-    Link(s1, h1)
-    Link(s1, h2)
-    Link(s1, h3)
-    Link(s1, h4)
+    net.addLink(s1, h1)
+    net.addLink(s1, h1)
+    net.addLink(s1, h2)
+    net.addLink(s1, h3)
+    net.addLink(s1, h4)
 
     net.build()
     c0.start()
